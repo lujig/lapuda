15625cf83508d0415336aff82322ea6d1a595768
lujig <lujig@pku.edu.cn>
2024-08-30 13:38:23 +0800

    new

--- doc/text.py
+++ doc/text.py
@@ -6,7 +6,7 @@ filename='text.txt'
 #
 class ldpara_text():
 	def __init__(self):
-		f=np.loadtxt(dirname+'/'+filename,dtype=str,delimiter='\t')
+		f=np.loadtxt(open(dirname+'/'+filename, encoding='utf8'),dtype=str,delimiter='\t')
 		f=f[f[:,0]=='ldpara']
 		for i in f:
 			self.__setattr__(str(i[1]),str(i[2]))
